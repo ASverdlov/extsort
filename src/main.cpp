36225c2c7c276d87d109bc13f2ce3db532fb32eb
Albert Sverdlov <sverdlovalbert@gmail.com>
2019-07-11 10:32:31 +0300

    Add --merge-concurrency and --chunksize options

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,11 @@
 using std::string;
 
 string temp_path;
+
 string input_path;
 string output_path;
-
-static const int INITIAL_CHUNKSIZE = 10;
-static const int MERGE_ASSOCIATIVITY = 3;
-static const int NUM_THREADS = 8;
+int initial_chunksize = 32768;
+int merge_concurrency = 8192;
 
 Chunk merge_and_write_back(Chunks chunks_to_merge) {
   int start = chunks_to_merge.front().start;
@@ -32,7 +31,7 @@ Chunk merge_and_write_back(Chunks chunks_to_merge) {
 }
 
 void run() {
-  auto chunks = divide(input_path, INITIAL_CHUNKSIZE);
+  auto chunks = divide(input_path, initial_chunksize);
   for (auto c : chunks) {
     sort(c, c);
   }
@@ -50,7 +49,7 @@ void run() {
     int chunk_id = 0;
     while (chunk_id < chunks.size()) {
       Chunks chunks_to_merge;
-      for (int i = 0; chunk_id < chunks.size() && i < MERGE_ASSOCIATIVITY; ++i) {
+      for (int i = 0; chunk_id < chunks.size() && i < merge_concurrency; ++i) {
         chunks_to_merge.push_back(chunks[chunk_id]);
         ++chunk_id;
       }
@@ -67,20 +66,25 @@ void parse_options(int argc, char* argv[]) {
   char ch;
 
   static option long_options[] = {
-    {"input",   required_argument, 0,  'i' },
-    {"output",  required_argument, 0,  'o' },
-    {"help",    no_argument,       0,  'h' },
-    {0,         0,                 0,  0 }
+    {"input",             required_argument, 0,  'i' },
+    {"output",            required_argument, 0,  'o' },
+    {"merge-concurrency", required_argument, 0,  'm' },
+    {"chunksize",         required_argument, 0,  'c' },
+    {"help",              no_argument,       0,  'h' },
+    {0,                   0,                 0,  0 }
   };
-  static const char *opts = "i:o:h";
+  static const char *opts = "hi:o:m:c:";
 
   while ((ch = getopt_long(argc, argv, opts, long_options, NULL)) != -1) {
     switch (ch) {
     case 'h':
       std::cout << "Help:" << std::endl;
-      std::cout << "--input, -i: input filename path" << std::endl;
-      std::cout << "--output, -o: input filename path" << std::endl;
-      std::cout << "--temp, -t: temporary directory" << std::endl;
+      std::cout << "--input, -i:\tinput filename path" << std::endl;
+      std::cout << "--output, -o:\tinput filename path" << std::endl;
+      std::cout << "--merge-concurrency, -m:\tmaximum number of chunks to be merged in parallel (1024 by default)" << std::endl;
+      std::cout << "--chunksize, -c:\tinitial chunks minimum size (1024 bytes by default)" << std::endl;
+
+      exit(0);
       break;
 
     case 'i':
@@ -91,6 +95,16 @@ void parse_options(int argc, char* argv[]) {
     case 'o':
       std::cout << "-o provided: " << string(optarg) << std::endl;
       output_path = optarg;
+      break;
+
+    case 'm':
+      std::cout << "-m provided: " << string(optarg) << std::endl;
+      merge_concurrency = atoi(optarg);
+      break;
+
+    case 'c':
+      std::cout << "-c provided: " << string(optarg) << std::endl;
+      initial_chunksize = atoi(optarg);
       break;
 
     default:
